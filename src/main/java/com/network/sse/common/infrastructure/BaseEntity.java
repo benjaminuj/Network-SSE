08b94d3e3417b8b038cb5c217d8176ed98b7df30
benjaminuj <chosj1526@gmail.com>
2024-12-12 20:24:26 +0900

    fix: 데이터 생성일자 관리 방법 수정

--- src/main/java/com/network/sse/common/infrastructure/BaseEntity.java
+++ src/main/java/com/network/sse/common/infrastructure/BaseEntity.java
@@ -2,18 +2,21 @@ package com.network.sse.common.infrastructure;
 
 import com.network.sse.common.domain.Status;
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import lombok.Getter;
-import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
-    @CreationTimestamp
+    @CreatedDate
     @Column(name = "createdAt", nullable = false, updatable = false)
     private LocalDateTime createdAt;
     @UpdateTimestamp
